f01ee07aa9c944c204e19a9907039d56f8e7d9ff
viaeoo <viaeoo@gmail.com>
2018-04-10 23:24:33 +0900

    app update

---
+++ src/shared/nav/nav-data.ts
@@ -0,0 +1,34 @@
+import { NavOptions } from './nav-options';
+
+export const NavData: NavOptions[] = [
+  {
+    page: '',
+    name: 'location',
+    icon: 'pin',
+    color: 'black'
+  },
+  {
+    page: '',
+    name: 'library',
+    icon: 'apps',
+    color: 'black'
+  },
+  {
+    page: '',
+    name: 'write',
+    icon: 'add-circle',
+    color: 'primary'
+  },
+  {
+    page: '',
+    name: 'member',
+    icon: 'person',
+    color: 'black'
+  },
+  {
+    page: '',
+    name: 'setting',
+    icon: 'settings',
+    color: 'black'
+  }
+];
---
+++ src/shared/nav/nav-options.ts
@@ -0,0 +1,6 @@
+export interface NavOptions {
+  page: string,
+  name: string,
+  icon: string,
+  color: string
+}
