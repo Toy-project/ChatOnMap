a8f15b88ca276e269446b0ce43977837641b184f
viaeoo <viaeoo@gmail.com>
2018-04-19 16:52:04 +0900

    modify mypage layout

--- src/components/components.module.ts
+++ src/components/components.module.ts
@@ -2,25 +2,16 @@ import { NgModule } from '@angular/core';
 import { IonicModule } from 'ionic-angular';
 
 import { LayHeaderComponent } from './lay-header/lay-header';
-import { MyFriendsComponent } from './my-friends/my-friends';
-import { MyActivityComponent } from './my-activity/my-activity';
-import { MyProfileComponent } from './my-profile/my-profile';
 
 @NgModule({
 	declarations: [
 		LayHeaderComponent,
-		MyFriendsComponent,
-		MyActivityComponent,
-		MyProfileComponent
 	],
 	imports: [
 		IonicModule
 	],
 	exports: [
 		LayHeaderComponent,
-		MyFriendsComponent,
-		MyActivityComponent,
-		MyProfileComponent
 	]
 })
 export class ComponentsModule {}
